314df54fced30b2a081c43d54bdb9456a036358d
Irwin Razaghi <irwin@terem.com.au>
2019-07-22 20:18:56 +0800

    Introduced Boom.  Created middleware to handle Boom exceptions.

--- src/server.mjs
+++ src/server.mjs
@@ -1,12 +1,16 @@
 import express from "express";
 
+import exceptions from "./middlewares/exceptions";
 import routes from "./routes";
 
 export default class Server {
   constructor() {
     this.app = express();
 
+    this.app.use(exceptions);
     this.app.use(routes);
+
+    this.app.use(exceptions);
   }
 
   async listen(port) {
